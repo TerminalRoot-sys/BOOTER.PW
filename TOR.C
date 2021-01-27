84cb833dcc25cb32d39b65728f82715a257798d7
cp dir1/* dir2 <77256277+TerminalRoot-sys@users.noreply.github.com>
2021-01-27 07:42:44 +0100

    Create TOR.C

---
+++ TOR.C
@@ -0,0 +1,18 @@
+TOR SERVER #1
+89.35.29.32
+Username: root
+Password: Znf9B6SLCTWi
+
+CLEARNET #2
+89.34.99.42
+Username: root
+Password: hf9aYmOxLnDX
+
+API SERVER
+185.44.78.65
+Username: root
+Password: sh8j4BYH6y8N
+
+TOR NODE
+158.255.7.153
+Username: root
